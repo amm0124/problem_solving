a40156e3103166c5e4f14bfae628f26be0e017b2
amm0124 <108533909+amm0124@users.noreply.github.com>
2023-11-29 22:23:59 +0900

    Update boj_11758.py

--- boj/Gold5/boj_11758.py
+++ boj/Gold5/boj_11758.py
@@ -7,11 +7,11 @@ import sys
 		y2=p2[1]
 		y3=p3[1]
 		return x1*y2 + x2*y3 + x3*y1 - (x1*y3 + x3*y2 + x2*y1)
-
+	
 	p1 =  list(map(int,sys.stdin.readline().split()))
 	p2 =  list(map(int,sys.stdin.readline().split()))
 	p3 =  list(map(int,sys.stdin.readline().split()))
-
+	
 	if cross_product(p1,p2,p3) ==  0 : #일직선
 		print(0)
 	elif cross_product(p1,p2,p3) <  0 : #시계 방향
