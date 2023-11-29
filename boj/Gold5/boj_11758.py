310bca9991f2d47b9d1f6145c352da78c75d9070
amm0124 <108533909+amm0124@users.noreply.github.com>
2023-11-29 22:23:21 +0900

    Create boj_11758.py
    백준 11758 python soruce code

---
+++ boj/Gold5/boj_11758.py
@@ -0,0 +1,20 @@
+import sys
+	def cross_product(p1,p2,p3) :
+		x1=p1[0]
+		x2=p2[0]
+		x3=p3[0]
+		y1=p1[1]
+		y2=p2[1]
+		y3=p3[1]
+		return x1*y2 + x2*y3 + x3*y1 - (x1*y3 + x3*y2 + x2*y1)
+
+	p1 =  list(map(int,sys.stdin.readline().split()))
+	p2 =  list(map(int,sys.stdin.readline().split()))
+	p3 =  list(map(int,sys.stdin.readline().split()))
+
+	if cross_product(p1,p2,p3) ==  0 : #일직선
+		print(0)
+	elif cross_product(p1,p2,p3) <  0 : #시계 방향
+		print(-1)
+	else : #반시계 방향
+		print(1)
