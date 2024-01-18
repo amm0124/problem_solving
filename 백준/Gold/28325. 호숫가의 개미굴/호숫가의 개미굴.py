c5f6e790a0d0d4d34328b0a7b2608da7d589c6ee
amm0124 <108533909+amm0124@users.noreply.github.com>
2024-01-18 16:17:02 +0900

    [Gold V] Title: 호숫가의 개미굴, Time: 452 ms, Memory: 86276 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.py
+++ 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.py
@@ -1,12 +1,20 @@
-import sys
+import sys 
+
+def solution(a,b) :
+    global n,C,dp
+    dp[1][0]=a
+    dp[1][1]=b
+    
+    for room in range(2,n+1) :
+        dp[room][0]=dp[room-1][1]+1
+        dp[room][1]=max(dp[room-1][0],dp[room-1][1]) + C[room]
+    
+    if a==0 : #첫 방 선택하지 않음
+        return max(dp[n][0],dp[n][1])
+    else : #첫 방 선택한 경우
+        return dp[n][1]
 
 n=int(input())
 C=[0]+list(map(int,sys.stdin.readline().split())) #idx가 방의 번호를 나타냄
 dp=[[0,0] for _ in range(n+1)]
-dp[1][0]=1
-dp[1][1]=C[1]
-for room in range(2,n+1) :
-    dp[room][0]=dp[room-1][1]+1
-    dp[room][1]=max(dp[room-1][0],dp[room-1][1]) + C[room]
-    
-print(max(dp[n][0], dp[n][1]))
+print(max(solution(0,C[1]), solution(1,0)))
