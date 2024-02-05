67625d03c3811682ea4f488e781627bd113c3440
amm0124 <108533909+amm0124@users.noreply.github.com>
2024-02-05 17:37:55 +0900

    [Gold V] Title: 퇴사 2, Time: 488 ms, Memory: 122544 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -1,15 +1,12 @@
+import sys 
+
 n=int(input())
-arr=[0]*(n+1)
-dp=[0]*(n+1)
-for i in range(1,n+1) : 
-    t,p=map(int,input().split())
-    arr[i]=[t,p]
-    
-for today in range(1,n+1) :
-    future=today+arr[today][0]-1
+dp=[0 for _ in range(n+1)]
+for today in range(1,n+1) : 
+    t,p=map(int ,sys.stdin.readline().split())
+    future=today+t-1
     max_profit=dp[today-1] #dp[today-1]    
     if future<=n :
-        dp[future]=max(dp[future], max_profit + arr[today][1])
+        dp[future]=max(dp[future], max_profit + p)
     dp[today]=max(max_profit, dp[today])
-    
 print(dp[n])
