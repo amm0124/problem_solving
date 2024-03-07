5305e329272ee787f68766e05573dcaf16a68df9
amm0124 <108533909+amm0124@users.noreply.github.com>
2024-03-07 21:36:44 +0900

    [Silver I] Title: 숨바꼭질, Time: 164 ms, Memory: 115652 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,24 +1,21 @@
-import sys,math
+import sys
 from collections import deque
 
 n,k=map(int,sys.stdin.readline().split())
 queue=deque()    
 queue.append((n,0))
-dp=[math.inf for _ in range(100001)]
+dp=[False for _ in range(100001)]
 while queue :
     position, weight = queue.popleft()
     if position==k :
         print(weight)
         sys.exit()
-    if position-1>=0 :
-        if weight+1<dp[position-1] : 
-            dp[position]=weight+1
-            queue.append((position-1,weight+1))
-    if position+1<=100000 :
-        if weight+1<dp[position+1] : 
-            dp[position]=weight+1 
-            queue.append((position+1,weight+1))
-    if 2*position<=100000 and 2*position>=0 :
-        if weight+1<dp[position*2] : 
-            dp[position]=weight+1
-            queue.append((2*position,weight+1))
+    if position-1>=0 and dp[position-1]==False:
+        queue.append((position-1,weight+1))
+        dp[position-1]=True
+    if position+1<=100000 and dp[position+1]==False:
+        dp[position+1]=True
+        queue.append((position+1,weight+1))
+    if (2*position<=100000 and 2*position>=0) and dp[position*2]==False :
+        dp[2*position]=True
+        queue.append((2*position,weight+1))
